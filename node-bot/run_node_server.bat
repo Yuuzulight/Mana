5eb2684eb9be43e041dd61fa5382d56aa195785f
Yuuzulight <yuuzulight@gmail.com>
2026-06-19 01:34:50 +0800

    Add backend TTS hooks and whisper fallback
    Signed-off-by: Yuuzulight <yuuzulight@gmail.com>

--- node-bot/run_node_server.bat
+++ node-bot/run_node_server.bat
@@ -1,6 +1,8 @@
 @echo off
 
 set "WHISPER_BIN=C:\ManaAI\Mana\tools\whisper\Release\whisper-cli.exe"
+if not exist "%WHISPER_BIN%" set "WHISPER_BIN=C:\ManaAI\Mana\tools\whisper\whisper-cli.exe"
+if not exist "%WHISPER_BIN%" set "WHISPER_BIN=C:\ManaAI\Mana\tools\whisper\main.exe"
 
 set "WHISPER_MODEL=C:\ManaAI\Mana\tools\whisper\models\ggml-tiny.en.bin"
 
