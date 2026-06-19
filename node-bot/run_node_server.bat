e662744e1e5669d1dd53be450caa93b9d78e1212
Yuuzulight <yuuzulight@gmail.com>
2026-06-19 18:10:14 +0800

    Wire local llama replies into Mana runtime

--- node-bot/run_node_server.bat
+++ node-bot/run_node_server.bat
@@ -5,6 +5,8 @@ if not exist "%WHISPER_BIN%" set "WHISPER_BIN=C:\ManaAI\Mana\tools\whisper\whisp
 if not exist "%WHISPER_BIN%" set "WHISPER_BIN=C:\ManaAI\Mana\tools\whisper\main.exe"
 
 set "WHISPER_MODEL=C:\ManaAI\Mana\tools\whisper\models\ggml-tiny.en.bin"
+set "TTS_PROVIDER=chatterbox"
+set "CHATTERBOX_TTS_URL=http://127.0.0.1:5010"
 
 cd /d "%~dp0"
 
